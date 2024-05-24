f0b8c72cff152b4d721c689a0e5bfcd5d9935d14
tomnyson <tabletkindfire@gmail.com>
2024-05-24 11:24:08 +0700

    crud user

--- ca1/bai3/users/create.php
+++ ca1/bai3/users/create.php
@@ -1,16 +1,5 @@
 <?php
-include_once('../DBUtil.php');
-$dbHelper = new DBUntil();
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     if (!isset($_POST["name"]) || empty($_POST['name'])) {
-//         $errors['name'] = "error";
-//     } else {
-//         $update = $dbHelper->update('categories', array('name' => $_POST['name']), "id=$_POST[id]");
-//         header("Location: index.php");
-//     }
-// }
-
-
+include('./handle_form.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,59 +11,16 @@ $dbHelper = new DBUntil();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+<style>
+.error {
+    color: red;
+}
+</style>
 
 <body>
     <div class="container mt-3">
         <h2>Thêm người dùng</h2>
-        <form action="update.php" method="post">
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">username</label>
-                <input class="form-control" type="text" name="username" placeholder="username">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">password</label>
-                <input class="form-control" type="password" name="password" placeholder="password">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">email</label>
-                <input class="form-control" type="email" name="email" placeholder="email">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">address</label>
-                <input class="form-control" type="text" name="address" placeholder="address">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">phone</label>
-                <input class="form-control" type="text" name="phone" placeholder="phone">
-            </div>
-            <div class="mb-3">
-                <select class="form-select" aria-label="Default select example">
-                    <option value="">Chọn vai trò</option>
-                    <option value="1">Admin</option>
-                    <option value="0">User</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label class="form-check-label mt-3">Trạng thái tài khoản:</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="status" value="1" id="status1" checked>
-                    <label class="form-check-label" for="status1">
-                        cho phép hoạt động
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="status" value="0" id="status2">
-                    <label class="form-check-label" for="flexRadioDefault2">
-                        khóa tài khoản
-                    </label>
-                </div>
-            </div>
-            <input type="submit" class="btn btn-success mt-3" value="Change">
-            <?php if (isset($errors['name'])) {
-                echo "<br/>";
-                echo "<span class='txt-danger'>$errors[name]</span>";
-            } ?>
-        </form>
+        <?php include_once('./form.php'); ?>
 </body>
 
 </html>
---
+++ ca1/bai3/users/handle_form.php
@@ -0,0 +1,56 @@
+<?php
+include_once('../DBUtil.php');
+$dbHelper = new DBUntil();
+
+function isVietnamesePhoneNumber($number)
+{
+    return preg_match('/^(03|05|07|08|09|01[2689])[0-9]{8}$/', $number) === 1;
+}
+
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    var_dump($_POST);
+    if (!isset($_POST['username']) || empty($_POST['username'])) {
+        $errors['username'] = "username is required";
+    } else {
+        $username = $_POST['username'];
+    }
+    if (!isset($_POST['email']) || empty($_POST['email'])) {
+        $errors['email'] = "email is required";
+    } else {
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "Invalid email format";
+        } else {
+            $email = $_POST['email'];
+        }
+    }
+    if (!isset($_POST['password']) || empty($_POST['password'])) {
+        $errors['password'] = "password is required";
+    } else {
+        $password = $_POST['password'];
+    }
+    if (!isset($_POST['address']) || empty($_POST['address'])) {
+        $errors['address'] = "address is required";
+    } else {
+        $address = $_POST['address'];
+    }
+    if (!isset($_POST['phone']) || empty($_POST['phone'])) {
+        $errors['phone'] = "phone is required";
+    } else {
+        if (!isVietnamesePhoneNumber($_POST['phone'])) {
+            $errors['phone'] = "phone number not correctly formatted";
+        } else {
+            $phone = $_POST['phone'];
+        }
+    }
+    if (!isset($_POST['role']) || empty($_POST['role'])) {
+        $errors['role'] = "role is required";
+    } else {
+        $role = $_POST['role'];
+    }
+    if (!isset($_POST['status']) || empty($_POST['status'])) {
+        $errors['status'] = "status is required";
+    } else {
+        $role = $_POST['status'];
+    }
+}
