97dcb05369b6fd922364ce880462a1840a9021f6
tomnyson <tabletkindfire@gmail.com>
2024-06-01 15:40:50 +0700

    register page

--- ca1/dashboard/includes/header.php
+++ ca1/dashboard/includes/header.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+include('config.php');
+var_dump(asset_url('css/sb-admin-2.min.css'));
+?>
 
 <head>
 
@@ -12,12 +16,10 @@
     <title>SB Admin 2 - Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="<?php asset_url('/vendor/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?php echo asset_url("css/sb-admin-2.min.css") ?>" rel="stylesheet">
 
 </head>
